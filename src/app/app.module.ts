07703a819c1febe3a2ea8427cfc6a414bb725363
dim2019 <dmitriy.mezvrishvili@mail.ru>
2022-03-08 19:09:45 +0400

    changed design and also deleted two component and added one new component maps, added responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrognoziComponent } from './features/prognozi/prognozi.component';
-import { HeaderComponent } from './features/header/header.component';
-import { FooterComponent } from './features/footer/footer.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+import { MapsComponent } from './features/maps/maps.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     PrognoziComponent,
-    HeaderComponent,
-    FooterComponent
+    MapsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/features/prognozi/prognozi.component.ts
+++ src/app/features/prognozi/prognozi.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrognoziComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
 
   ngOnInit(): void {
+    this._http.get<any>('https://thingproxy.freeboard.io/fetch/https://api.openweathermap.org/data/2.5/weather?q=gori&appid=299fb2133133f9d8fc214f5ae28ca753').subscribe(res=>{
+      console.log(res);
+    })
   }
 
 }
